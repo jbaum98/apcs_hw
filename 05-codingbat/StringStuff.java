3174f12ad6f45517cdb59473920f16b38aebb262
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-09-24 09:22:37 -0400

    classwork lab

--- 05-codingbat/StringStuff.java
+++ 05-codingbat/StringStuff.java
@@ -12,4 +12,22 @@ public class StringStuff {
             return (n - 21) * 2;
         }
     }
+    public static boolean nearHundred(int n){
+        return Math.abs(n-100)<=10 || Math.abs(n-200)<=10;
+    }
+    public static boolean mixStart(String str) {
+        return str.substring(1,3).equals("ix");
+    }
+    public static int teaParty(int tea, int candy) {
+        if (tea >= 5 && candy >= 5) {
+            if (tea >= 2 * candy || candy >= 2 * tea)
+                return 2;
+            else
+                return 1;
+        } else
+            return 0;
+    }
+    public boolean lastDigit(int a, int b, int c) {
+        return (a%10==b%10)||(a%10==c%10)||(b%10==c%10);
+    }
 }
