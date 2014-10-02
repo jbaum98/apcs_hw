a1c0bd82f43a212af831a4d6e8bd9033190dd90e
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-10-02 17:27:44 -0400

    hw07b add tri3 diamond and tri4

--- 07-shapes/Driver.java
+++ 07-shapes/Driver.java
@@ -6,5 +6,11 @@ public class Driver {
 	System.out.println(s.box(4,5));
     System.out.println(s.tri1(4));
     System.out.println(s.tri2(4));
+    System.out.println(s.tri3(4));
+    System.out.println(s.diamond(4));
+    System.out.println(s.diamond(5));
+    System.out.println(s.diamond(13));
+    System.out.println(s.diamond(14));
+    System.out.println(s.tri4(4));
     }
 }
--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -1,8 +1,7 @@
 public class Shapes {
     public String box(int r, int c) {
         // 3 minutes
-        String s = "";
-
+        String s = ""; 
         while (r>0){
             int cc = 0;
             while (cc < c){
@@ -49,6 +48,58 @@ public class Shapes {
             }
             out += "\n";
             row++;
+        }
+        return out;
+    }
+
+    public String tri3(int h) {
+        //6 minutes
+        String out = new String();
+        int row, cursor; 
+        for (row = 0; row < h; row++) {
+            int length = h + row;
+            int first_star = h - row;
+            for (cursor = 1; cursor <= length; cursor++) {
+                String chr = cursor < first_star ? " " : "*";
+                out += chr;
+            }
+            out += "\n";
+        }
+        return out;
+    }
+
+    public String diamond(int h) {
+        //15 minutes
+        String out = new String();
+        int y, cursor;
+        int middle = h % 2 == 0 ? h - 1 : h;
+        int max = h % 2 == 0 ? h/2-1 : (h-1)/2;
+        for (y = max; y >= -1*max; y--) {
+            String line = new String();
+            // first_star = |y|
+            int length = (middle+1)/2+max-Math.abs(y);
+            for (cursor = 0; cursor < length; cursor++) {
+                String chr = cursor < Math.abs(y) ? " " : "*";
+                line += chr;
+            }
+            if (y == 0 && h % 2 == 0)
+                out+= line + "\n";
+            out += line + "\n";
+        }
+        return out;
+    }
+
+    public String tri4(int h) {
+        //10 minutes
+        String out = new String();
+        int row, cursor;
+        for (row = 0; row < h; row++) {
+            // first_star = row;
+            for (cursor = 0; cursor < h; cursor++) {
+                String chr = cursor < row ? " " : "*";
+                out += chr;
+            }
+            out += "\n";
         }
         return out;
     }
