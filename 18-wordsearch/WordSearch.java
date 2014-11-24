987ac67d518ff9b5e9b52544a214cbd4873e4356
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-11-24 00:00:52 -0500

    completely fix wordsearch

--- 18-wordsearch/WordSearch.java
+++ 18-wordsearch/WordSearch.java
@@ -1,9 +1,10 @@
+import java.util.*;
+
 public class WordSearch {
 
     private char[][] board;
 
-    public WordSearch(int r, int c){
-        board = new char[r][c];
+    public WordSearch(int r, int c){ board = new char[r][c];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j]='.';
@@ -26,96 +27,87 @@ public class WordSearch {
         return s;
     }
 
-    private void error(String err) {
-        System.out.println("Error: " + err);
+    private boolean check(int length, int row, int col, int d_y, int d_x) {
+        int max_row = board.length - 1;
+        int max_col = board[0].length - 1;
+        int final_row = row + d_y*(length-1);
+        int final_col = col + d_x*(length-1);
+        return
+            !(d_x == 0 && d_y == 0) // wouldn't make sense
+            &&
+            final_row <= max_row && final_row >= 0 // final row is good
+            &&
+            final_col <= max_col && final_col >= 0 // final row is good
+            &&
+            row <= max_row && row >= 0 // initial row is good
+            &&
+            col <= max_col && col >= 0 // initial col is good
+            ;
     }
 
-    private void checkStart(int row, int col) {
-        if ( !(row >= 0 && row < board.length) ) {
-            error("row doesn't exist");
-        } else if ( !(col >= 0 && col < board[0].length) ) {
-            error("column doesn't exist");
+    public void addWord(String word, int row, int col, int d_y, int d_x) {
+        /*
+            d_y and d_x are the "slope" of the word
+            for example:
+                horizontal:0, 1
+                vertical: 1, 0
+                horizontal backwards: 0, -1
+                diagonal to the bottom right: 1,  1
+            credit to Max Fishelson for suggestiong d_x d_y instead of seperate functions
+            but recursion was my idea :)
+        */
+        if (word.length() != 0) {
+            board[row][col] = word.charAt(0);
+            addWord(word.substring(1), row+d_y, col+d_x, d_y, d_x);
         }
     }
 
-    private String reverse(String s) {
-        String rev = "";
-        for (int i = s.length() ; i >= 0; i--) {
-            rev += s.charAt(i);
-        }
-        return rev;
-    }
+    public boolean addWord(String word) {
+        ArrayList<int[]> valids= new ArrayList<int[]>();
 
-    public void addWordH(String word, int row, int col) {
-        // check it fits
-        checkStart(row, col);
-        if (col + word.length() >= board[0].length) {
-            error("word is too long");
-        }
-
-        int c = col;
-        for (int i = 0; i < word.length(); i++) {
-            if (! (board[row][c] == '.' || board[row][c] == word.charAt(i)) ) {
-                error("Illegal overlap");
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                for (int d_y = -1; d_y <= 1; d_y++) {
+                    for (int d_x = -1; d_x <= 1; d_x++) {
+                        if (check(word.length(), row, col, d_y, d_x)) {
+                            valids.add(new int[] {row, col, d_y, d_x});
+                        }
+                    }
+                }
             }
-            c++;
         }
-        for (int i = 0; i < word.length(); i++) {
-            board[row][c] = word.charAt(i);
-            c++;
-        }
-    }
-
-    public void addWordHR(String word, int row, int col) {
-        addWordH(reverse(word), row, col-word.length());
-    };
-
-    public void addWordV(String word, int row, int col) {
-        // check it fits
-        checkStart(row, col);
-        if (row + word.length() >= board.length) {
-            error("word is too long");
-        }
-
-        int r = row;
-        for (int i = 0; i < word.length(); i++) {
-            if (! (board[r][col] == '.' || board[r][col] == word.charAt(i)) ) {
-                error("Illegal overlap");
+        /* // debug
+        for (int i = 0; i < valids.size(); i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(valids.get(i)[j]);
             }
-            r++;
+            System.out.print("\n");
         }
-        for (int i = 0; i < word.length(); i++) {
-            board[r][col] = word.charAt(i);
-            r++;
+        System.out.println(valids.size());
+        */
+        if (valids.size() == 0) {
+            return false;
+        } else {
+            Random r = new Random();
+            int[] choice = valids.get(
+                    r.nextInt(valids.size()) // random index
+                    );
+            /* //debug
+            for (int j = 0; j < 4; j++) {
+                System.out.print(choice[j]);
+            }
+            System.out.print("\n");
+            */
+            addWord(word, choice[0], choice[1], choice[2], choice[3]);
+            return true;
         }
     }
 
-    public void addWordVR(String word, int row, int col) {
-        addWordV(reverse(word), row-word.length(), col);
-    };
-
-
+    /* //debug
     public static void main(String[] args) {
-        WordSearch w= new WordSearch();
-        System.out.println(w);
-
-        // check it fits on board
-        w.addWordH("hello", 3, 15); //this should work
-        w.addWordH("hello", 56, 7); //this row doesn't exist
-        w.addWordH("hello", 7, 56); //this column doesn't exist
-        w.addWordH("hello", 7, 39); //doesn't fit
-        w.addWordV("hello", 3, 15); //this should work
-        w.addWordV("hello", 56, 7); //this row doesn't exist
-        w.addWordV("hello", 7, 56); //this column doesn't exist
-        w.addWordV("hello", 19, 9); //doesn't fit
-
-        // check overlapping
-        w.addWordH("hello", 3, 15); //this should work
-        w.addWordH("hello", 3, 16); //this overlaps wrong
-        w.addWordV("hello", 3, 15); //this works, using v
-        w.addWordH("hello", 4, 15); //this overlaps wrong
-
+        WordSearch w= new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        w.addWord(args[2]);
         System.out.println(w);
     }
+    */
 }
-
