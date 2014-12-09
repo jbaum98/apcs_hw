cc58f915bd890f7d3f6464f4e7feef0f2ba44fd1
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-12-09 22:35:04 +0000

    interval compareTo

--- 21-sorting_classes/Interval.java
+++ 21-sorting_classes/Interval.java
@@ -3,45 +3,55 @@ import java.util.*;
 
 class Interval {
 
-		private int low,high;
-		private Random r = new Random();				
-		private static int numIntervals = 0;
-		
-		public Interval(int l, int h){
-				low = l;
-				high = h;
-				numIntervals = numIntervals + 1;
-		}
-		
-		public Interval() {
-				int l = r.nextInt(100);
-				int h = l + 1 + r.nextInt(100);
-				low = l;
-				high = h;
-				numIntervals = numIntervals + 1;
-		}
+    private int low,high;
+    private Random r = new Random();
+    private static int numIntervals = 0;
 
-		public String toString() {
-				//String s = "Inteval: "+numIntervals+": "+ "["+low+","+high+"]";
-				String s = "["+low+","+high+"]";
+    public Interval(int l, int h){
+        low = l;
+        high = h;
+        numIntervals = numIntervals + 1;
+    }
 
-				return s;
-		}
+    public Interval() {
+        int l = r.nextInt(100);
+        int h = l + 1 + r.nextInt(100);
+        low = l;
+        high = h;
+        numIntervals = numIntervals + 1;
+    }
 
-		public static void printstuff() {
-				System.out.println("Stuff");
-		}
-		public static void main(String[] args) {
+    public String toString() {
+        // String s = "Inteval: "+numIntervals+": "+ "["+low+","+high+"]";
+        String s = "["+low+","+high+"]";
 
-				Interval ival = new Interval();
-				ival.printstuff(); // normal calling off of an instance
-				printstuff(); // since printStuff is static we can call it without an instance
-				Interval.printstuff(); // we can also call it right off a class
+        return s;
+    }
 
-				Interval[] a = new Interval[10];
-				for (int i = 0; i < a.length; i++) {
-						a[i] = new Interval();
-				}
-				System.out.println(Arrays.toString(a));
-		}
+    public static void printstuff() {
+        System.out.println("Stuff");
+    }
+
+    public int compareTo(Interval other) {
+        int diff = this.low - other.low;
+        if (diff == 0) {
+            return this.high - other.high;
+        } else {
+            return diff;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Interval ival = new Interval();
+        // ival.printstuff(); // normal calling off of an instance
+        printstuff(); // since printStuff is static we can call it without an instance
+        Interval.printstuff(); // we can also call it right off a class
+
+        Interval[] a = new Interval[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new Interval();
+        }
+        System.out.println(Arrays.toString(a));
+    }
 }
