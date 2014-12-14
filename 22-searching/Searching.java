330263061adc9bdfd747647ca95c400cf42e581a
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-12-14 15:51:10 -0500

    finished but not tested

--- 22-searching/Searching.java
+++ 22-searching/Searching.java
@@ -35,13 +35,44 @@ public class Searching {
         }
         return null;
     }
-
+    
     public Comparable bSearch(Comparable item) {
+      int first = 0;
+      int last = a.length;
+      while (first != last) {
+        int mid = (first + last) / 2;
+        if (a[mid].compareTo(item) > 0) {
+          first = mid;
+        } else {
+          last = mid;
+        }
+      }
+      if (equal(a[first], item)) {
         return null;
+      } else {
+        return first;
+      }
     }
 
     public Comparable rbSearch(Comparable item) {
-        return null;
+      return rbSearch(item, 0, a.length);
+    }
+    
+    private Comparable rbSearch (item, first, last) {
+      if (first == last) { // base case: 1 element list
+        if (equal(a[first], item)) {
+          return null;
+        } else {
+          return first;
+        }
+      } else { // recursive call depending on greater or less
+        int mid = (first + last) / 2;
+        if (a[mid].compareTo(item) > 0) {
+          return rbSearch(item, mid, last);
+        } else {
+          return rbSearch(item, first, mid);
+        }
+      }
     }
 
     public static void main(String[] args) {
